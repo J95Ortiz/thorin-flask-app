39baaa26d007433aff6bdcfd7cea720da4e1b4d5
Joe Ortiz <Se_sema_95@hotmail.com>
2024-08-18 15:15:33 +0000

    Created json file and linked to run.py & about page

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 # Imports os from the python library.
 import os
 
+# Imports the json library as we'll use this to pull the members' data
+import json
+
 # Imports the Flask class, and render_template which displays the file assigned to it.
 from flask import Flask, render_template     
 
@@ -22,18 +25,21 @@ def index():
 # This routing will open the index.html file
 @app.route("/about")     
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company = data)
 
 
 # This routing will open the contact.html file
 @app.route("/contact")     
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 # This routing will open the careers.html file
 @app.route("/careers")     
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 # "__main__" is the name of the default module in Python
